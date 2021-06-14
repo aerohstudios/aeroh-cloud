628b774dd97b274ee25ead01a3b2f96f9bcec6a6
Shiv Deepak <shivdeepak@gmail.com>
2021-06-13 19:06:36 -0700

    setting up devise for authentication

---
+++ app/controllers/concerns/devise_concern.rb
@@ -0,0 +1,5 @@
+module DeviseConcern
+    def after_sign_in_path_for(resource)
+        dashboard_index_path
+    end
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,9 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Application Specific Configuration
+
+  # Devise Needs this (possibly future functionality will need this as well)
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
