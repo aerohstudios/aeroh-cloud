2bebcfc1272409111126ccc134fab302394a6d3c
Shiv Deepak Muddada <shiv@aeroh.org>
2022-05-18 22:37:57 -0500

    use a single re-usable policy for iot things

--- app/services/thing_service/provisioner.rb
+++ app/services/thing_service/provisioner.rb
@@ -4,20 +4,19 @@ module ThingService
 
 
     class Provisioner < ThingService::Base
-        attr_reader :identifier, :thing, :policy, :certificate
+        attr_reader :identifier, :thing, :policy_name, :certificate
 
         def initialize(identifier)
             super()
 
             @identifier = identifier
             @thing = nil
-            @policy = nil
+            @policy_name = "#{Rails.env.capitalize}_Aeroh_Device_Access"
             @certificate = nil
         end
 
         def execute
             create_thing
-            create_policy
             create_certificate
 
             attach_thing_to_certificate
@@ -37,39 +36,6 @@ module ThingService
             @thing = @client.create_thing(thing_name: thing_name)
         end
 
-        def create_policy
-            raise StandardError.new("You need to create a thing first!") unless @thing
-
-            policy_name = "#{Rails.env.capitalize}_Aeroh_Device_Access_#{@identifier}"
-            policy_document = {
-                "Version": "2012-10-17",
-                "Statement": [
-                    {
-                    "Effect": "Allow",
-                    "Action": "iot:Connect",
-                    "Resource": @thing.thing_arn
-                    },
-                    {
-                    "Effect": "Allow",
-                    "Action": "iot:Publish",
-                    "Resource": @thing.thing_arn
-                    },
-                    {
-                    "Effect": "Allow",
-                    "Action": "iot:Subscribe",
-                    "Resource": @thing.thing_arn
-                    },
-                    {
-                    "Effect": "Allow",
-                    "Action": "iot:Receive",
-                    "Resource": @thing.thing_arn
-                    }
-                ]
-            }.to_json
-
-            @policy = @client.create_policy(policy_name: policy_name, policy_document: policy_document)
-        end
-
         def create_certificate
             @certificate = @client.create_keys_and_certificate(set_as_active: true)
         end
@@ -81,9 +47,9 @@ module ThingService
         end
 
         def attach_policy_to_certificate
-            raise StandardError.new("You need to create a policy and certificate first!") unless @policy and @certificate
+            raise StandardError.new("You need to create a certificate first!") unless @certificate
 
-            @client.attach_principal_policy(policy_name: @policy.policy_name, principal: @certificate.certificate_arn)
+            @client.attach_principal_policy(policy_name: @policy_name, principal: @certificate.certificate_arn)
         end
     end
 end
