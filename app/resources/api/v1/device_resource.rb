cd9f56391a5ed30e9fb1c44fa271974a585c3ba9
Shiv Deepak Muddada <shiv@aeroh.org>
2022-05-18 23:14:02 -0500

    adding thing_name to device resource

--- app/resources/api/v1/device_resource.rb
+++ app/resources/api/v1/device_resource.rb
@@ -1,8 +1,12 @@
 class Api::V1::DeviceResource < JSONAPI::Resource
-    attributes :name, :mac_addr, :provisioned, :cert_active, :certificate_arn, :certificate_pem, :certificate_public_key, :certificate_private_key
+    attributes :name, :mac_addr, :provisioned, :cert_active, :certificate_arn, :certificate_pem, :certificate_public_key, :certificate_private_key, :thing_name
 
     before_save do
         @model.user_id = context.current_user.id if @model.new_record?
+    end
+
+    def thing_name
+      @model.thing_arn.split("thing/").last
     end
 
     def self.records(options={})
@@ -11,11 +15,11 @@ class Api::V1::DeviceResource < JSONAPI::Resource
     end
 
     def self.updatable_fields(context)
-        super - [:mac_addr, :certificate_arn, :certificate_pem, :certificate_public_key, :certificate_private_key, :created_at, :updated_at]
+        super - [:mac_addr, :certificate_arn, :certificate_pem, :certificate_public_key, :certificate_private_key, :created_at, :updated_at, :thing_name]
     end
 
     def self.creatable_fields(context)
-        super - [:certificate_arn, :certificate_pem, :certificate_public_key, :certificate_private_key, :created_at, :updated_at]
+        super - [:certificate_arn, :certificate_pem, :certificate_public_key, :certificate_private_key, :created_at, :updated_at, :thing_name]
     end
 
     def fetchable_fields
