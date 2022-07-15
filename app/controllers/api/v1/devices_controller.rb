d26a432a7cddefb24a49b3a475aba8444322e8db
Shiv Deepak Muddada <shiv@aeroh.org>
2022-07-15 15:25:22 -0700

    enable device deletion through api without csrf token

--- app/controllers/api/v1/devices_controller.rb
+++ app/controllers/api/v1/devices_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::DevicesController < ApplicationController
 
     before_action :authorize_api_endpoint
-    skip_before_action :verify_authenticity_token, only: [:create]
+    skip_before_action :verify_authenticity_token, only: [:create, :destroy]
 end
