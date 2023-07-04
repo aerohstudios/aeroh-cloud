c65003ee0a653ba2d51dc3399dd593fe803551b6
Shiv Deepak Muddada <shiv@aeroh.org>
2023-07-03 18:25:24 -0700

    adding login flow to retrieve access_token for mobile client authentication

--- app/controllers/concerns/devise_concern.rb
+++ app/controllers/concerns/devise_concern.rb
@@ -18,4 +18,74 @@ module DeviseConcern
             session[:devise_return_to] = params["return_to"]
         end
     end
+
+    def mobile_access_token_request_valid?
+        # check client id
+        @oauth_app = Doorkeeper::Application.where(uid: params[:client_id]).first
+        unless @oauth_app.present?
+            render json: { errors: ["Request made from unknown application!"], data: {} }, status: :unauthorized
+            return false
+        end
+
+        # check verify signature
+        @email = params[:email].to_s
+        @password = params[:password].to_s
+        @scopes = params[:scopes].to_s
+        request_timestamp = params[:timestamp].to_i
+
+        payload = {
+            email: @email,
+            password: @password,
+            scopes: @scopes,
+            timestamp: request_timestamp
+        }.to_json
+
+        expected_signature = params[:signature].to_s
+        actual_signature = generate_hmac(payload, @oauth_app.secret)
+
+        if expected_signature != actual_signature
+            render json: { errors: ["Request signature is invalid!"], data: {} }, status: :unauthorized
+            return false
+        end
+
+        # check request timestamp is within last 5 seconds
+        current_timestamp = Time.now.to_i
+
+        if current_timestamp < request_timestamp or request_timestamp < current_timestamp - 5
+            render json: { errors: ["Request timestamp is invalid! Server Time: #{current_timestamp}"], data: {} }, status: :unauthorized
+            return false
+        end
+
+        # verify scope
+        if @scopes != "mobile"
+            render json: { errors: ["Request scope is invalid"], data: {} }, status: :unauthorized
+            return false
+        end
+
+        return true
+    end
+
+    def generate_mobile_access_token
+        access_token = Doorkeeper::AccessToken.create!(
+            resource_owner_id: @user.id,
+            application_id: @oauth_app.id,
+            expires_in: Doorkeeper.configuration.access_token_expires_in,
+            scopes: @scopes
+        )
+
+        render json: {
+            data: {
+                access_token: access_token.token,
+                refresh_token: access_token.refresh_token,
+                expires_in: access_token.expires_in,
+                created_at: access_token.created_at,
+            }
+        }
+    end
+
+    def generate_hmac(data, key)
+        digest = OpenSSL::Digest.new('sha256')
+        hmac = OpenSSL::HMAC.digest(digest, key, data)
+        return hmac.unpack1('H*') # Returns hexadecimal representation
+    end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  protect_from_forgery except: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -9,9 +10,25 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    if ['application/vnd.api+json', 'application/json'].include? request.content_type
+      if mobile_access_token_request_valid?
+        @user = User.find_by(email: @email)
+        if @user.present? and @user.valid_password?(@password)
+          generate_mobile_access_token
+        else
+          render json: { errors: ["Email or Password is not valid!"], data: {} }, status: :unauthorized
+          return false
+        end
+      end
+    else
+      if verify_authenticity_token != false
+        super
+      else
+        raise ActionController::InvalidAuthenticityToken
+      end
+    end
+  end
 
   # DELETE /resource/sign_out
   # def destroy
