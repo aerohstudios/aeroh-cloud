4460d4f0e87460cdb58ffd73e11ee79a509f56af
Shiv Deepak Muddada <shiv@aeroh.org>
2023-10-04 14:44:31 -0700

    serve /apple-app-site-association route

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,17 @@ class HomeController < ApplicationController
 
     def index
     end
+
+    def apple_app_site_association
+        render :json => {
+            "applinks": {},
+            "activitycontinuation": {},
+            "webcredentials": {
+                "apps": [
+                    "org.aeroh.AerohLink"
+                ]
+            },
+            "appclips": {}
+        }
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "home#index"
+  get '/apple-app-site-association' => 'home#apple_app_site_association'
 
   namespace :api do
     namespace :v1 do
